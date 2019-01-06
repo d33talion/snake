d4d1985a09a74d7e2416dbc0cca340b259af237c
d33talion <d33talion@yandex.ru>
2019-01-06 20:07:51 +0300

    Comments were removed

--- conf.lua
+++ conf.lua
@@ -1,5 +1,4 @@
-function love.conf(t)--Функция которую вызывает love перед стартом приложения
-    io.stdout:setvbuf("no")--Из-за особенностей Sublime Text, точнее его консольки нужно вызывать эту функцию, 
-                           --иначе все что мы выводим с помощью print(str) бкдет выведено после закрытия игры.
+function love.conf(t)
+    io.stdout:setvbuf("no")
     t.title = "Snake is so awesome!"
 end
--- main.lua
+++ main.lua
@@ -1,40 +1,40 @@
 local apple, snake, direction, width, height, delay
 
 function love.load()
-	width = love.graphics.getWidth() / 10			--функция возвращает длину окна на данный момент
-	print("Width " .. width)          				--дебаг вывод
-	height = love.graphics.getHeight() /10			--тоже самое но с высотой
+	width = love.graphics.getWidth() / 10
+	print("Width " .. width)
+	height = love.graphics.getHeight() /10
 	print("Height " .. height)
-	apple = { x = math.random(width);				--теперь нам нужно яблоко, генерируем X-координату
-			  y = math.random(height); } 			--и Y..
-	snake = { } 									--инициализируем таблицу с координатами змейки
-	for i = 1, 10 do 								--первые 10 клеток надо с чего-то начинать
-		point = { x = 10;							--и снова X..
-				  y = 10 + i; }						--и Y..
-		table.insert(snake, point)					--вписываем в таблицу
+	apple = { x = math.random(width);
+			  y = math.random(height); }
+	snake = { }
+	for i = 1, 10 do
+		point = { x = 10;
+				  y = 10 + i; }
+		table.insert(snake, point)
 	end
-	direction = "up"								--змейка должна куда то ползти
-	delay = 0										--змейка не должна быть нервной
+	direction = "up"
+	delay = 0
 end
 
 function love.update(dt)
-	for k, v in pairs(snake) do  					--Проверяем не съела-ла змейка саму себя
+	for k, v in pairs(snake) do
 		if k ~= 1 and snake[1].x == v.x and
 					  snake[1].y == v.y then
-			print("You died :3")					--смотрим на все с улыбкой (:
+			print("You died :3")
 			love.event.quit()
 		end
 	end
-	if delay % 2 == 0 then							--лечим нервы
+	if delay % 2 == 0 then
 		if apple.x == snake[1].x and
 		   apple.y == snake[1].y then
-				apple = { x = math.random(width);		--вот и поели, генерируем X-координату
-						  y = math.random(height); } 	--и Y..
-		else 											--не сразу ясно, но понятно
-			table.remove(snake, table.getn(snake)) 		--убираем последний элемент и добавляем его в начало
+				apple = { x = math.random(width);
+						  y = math.random(height); }
+		else
+			table.remove(snake, table.getn(snake))
 		end
 
-		if direction == "up" then					--ну тут вроде все ясно, обрабатываем направление
+		if direction == "up" then
 			table.insert(snake, 1, { x = snake[1].x;
 									 y = (snake[1].y - 1) % height; })
 		elseif direction == "down" then
@@ -51,8 +51,7 @@ function love.update(dt)
 	delay = delay + 1
 end
 
-function love.keypressed(key)						--здесь мы будем смотреть какая кнопка была нажата
-													--выглядит стремно, но правильно
+function love.keypressed(key)
 	if key == "up" then
 		direction = "up"
 	elseif key == "down" then
@@ -67,11 +66,11 @@ function love.keypressed(key)						--здесь мы будем смотреть
 end
 
 function love.draw()
-	love.graphics.setBackgroundColor(0, 0, 0)		--чистим экран
-	love.graphics.setColor(0, 0, 255)				--змея будет синей
-	for k, v in pairs(snake) do 					--отрисовываем каждую клетку
+	love.graphics.setBackgroundColor(0, 0, 0)
+	love.graphics.setColor(0, 0, 255)
+	for k, v in pairs(snake) do
 		love.graphics.rectangle("fill", v.x * 10, v.y * 10, 10, 10)
 	end
-	love.graphics.setColor(255, 255, 0)				--мне нравиться голд
+	love.graphics.setColor(255, 255, 0)
 	love.graphics.rectangle("fill", apple.x * 10, apple.y * 10, 10, 10)
 end
